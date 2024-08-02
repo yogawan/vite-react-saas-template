e59f99b87d16dda679cf686da1db9acc9496f626
Yogawan Aditya Pratama <123430193+yogawan@users.noreply.github.com>
2024-08-02 20:02:04 +0700

    update berberapa folder dan nama file api

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './sass/main.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './sass/main.scss';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -16,5 +16,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
